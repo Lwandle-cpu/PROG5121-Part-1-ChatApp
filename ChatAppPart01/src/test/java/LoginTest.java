60bac6641abfc19cda93d0bf8d3671482f760040
Lwandle <nolwandlemazibuko359@gmail.com>
2026-09-08 21:34:01 +0200

    commit changes

--- ChatAppPart01/src/test/java/LoginTest.java
+++ ChatAppPart01/src/test/java/LoginTest.java
@@ -7,6 +7,38 @@
  *
  * @author Student
  */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class LoginTest {
-    
+
+    @Test
+    public void testUsernameValid() {
+
+        // Arrange
+        Login login = new Login();
+        String username = "kyl_123";
+
+        // Act
+        boolean actual = login.checkUserName(username);
+
+        // Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testUsernameInvalid() {
+
+        // Arrange
+        Login login = new Login();
+        String username = "kyle!!!!";
+
+        // Act
+        boolean actual = login.checkUserName(username);
+
+        // Assert
+        assertFalse(actual);
+    }
 }
+
